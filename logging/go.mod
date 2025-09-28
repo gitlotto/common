e01ef245c3d708b046dabbd525b431b60b232369
Tigran Ohanyan <tigranohanyan777@gmail.com>
2025-09-29 00:41:10 +0400

    feat: log trace id (#44)

--- logging/go.mod
+++ logging/go.mod
@@ -2,9 +2,16 @@ module github.com/gitlotto/common/logging
 
 go 1.24.6
 
-require go.uber.org/zap v1.27.0
+require (
+	go.opentelemetry.io/otel v1.38.0
+	go.opentelemetry.io/otel/trace v1.38.0
+	go.uber.org/zap v1.27.0
+)
 
 require (
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 )
--- logging/logging.go
+++ logging/logging.go
@@ -1,11 +1,17 @@
 package logging
 
 import (
+	"context"
 	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
 )
+
+const AmznTraceIdFieldName = "X-Amzn-Trace-Id"
 
 func MustCreateZuluTimeLogger() (logger *zap.Logger) {
 
@@ -22,4 +28,22 @@ func MustCreateZuluTimeLogger() (logger *zap.Logger) {
 		panic(err)
 	}
 	return logger
+}
+
+
+func WithTraceID(ctx context.Context, logger *zap.Logger) (loggerOut *zap.Logger) {
+	loggerOut = logger
+	
+	baggage := make(propagation.MapCarrier)
+	otel.GetTextMapPropagator().Inject(ctx, propagation.MapCarrier(baggage))
+	
+	if amznTraceId, ok := baggage[AmznTraceIdFieldName]; ok {
+		loggerOut = loggerOut.With(zap.String(AmznTraceIdFieldName, amznTraceId))
+	}
+
+	spanContext := trace.SpanContextFromContext(ctx)
+	
+	loggerOut = loggerOut.With(zap.String("trace_id", spanContext.TraceID().String()))
+	return loggerOut
+
 }
